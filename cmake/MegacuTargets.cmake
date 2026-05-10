a8ac71815f4e04287eaa17046df701bc638301ff
Youwei Xiao <shallwexiao@gmail.com>
2026-05-11 00:12:54 +0800

    build: remove stale dispatcher scheduler component args

--- cmake/MegacuTargets.cmake
+++ cmake/MegacuTargets.cmake
@@ -1,11 +1,11 @@
 function(megacu_add_components)
-  set(one_value_args NAME DISPATCHER SCHEDULER PLATFORM BACKEND)
+  set(one_value_args NAME PLATFORM BACKEND)
   cmake_parse_arguments(MEGACU "" "${one_value_args}" "" ${ARGN})
 
   if(NOT MEGACU_NAME)
     message(FATAL_ERROR "megacu_add_components requires NAME")
   endif()
-  foreach(required DISPATCHER SCHEDULER PLATFORM BACKEND)
+  foreach(required PLATFORM BACKEND)
     if(NOT MEGACU_${required})
       message(FATAL_ERROR "megacu_add_components requires ${required}")
     endif()
@@ -16,13 +16,9 @@ function(megacu_add_components)
     ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../src/backends/nvshmem/backend.cc)
   target_link_libraries(${MEGACU_NAME} PUBLIC megacu_headers)
   target_compile_definitions(${MEGACU_NAME} PRIVATE
-    "MEGACU_COMPONENT_DISPATCHER=\"${MEGACU_DISPATCHER}\""
-    "MEGACU_COMPONENT_SCHEDULER=\"${MEGACU_SCHEDULER}\""
     "MEGACU_COMPONENT_PLATFORM=\"${MEGACU_PLATFORM}\""
     "MEGACU_COMPONENT_BACKEND=\"${MEGACU_BACKEND}\"")
   set_target_properties(${MEGACU_NAME} PROPERTIES
-    MEGACU_DISPATCHER "${MEGACU_DISPATCHER}"
-    MEGACU_SCHEDULER "${MEGACU_SCHEDULER}"
     MEGACU_PLATFORM "${MEGACU_PLATFORM}"
     MEGACU_BACKEND "${MEGACU_BACKEND}")
 endfunction()
